c35ca40c1852e80d24a3b083334b3f60c703d91c
antoniogrillo <ntngrillo@gmail.com>
2016-02-05 14:40:56 +0100

    mod 20160205

--- fabCR.py
+++ fabCR.py
@@ -29,13 +29,9 @@ def leggi_carta ():
         except nxppy.SelectError:
             pass
     time.sleep(1)
-
-def dbConnect ():
-    print '-Connessioe al DB-'
-    print '-DB Connesso-'
     
 def creaUtente():
-    print '-Crea Utente-'
+    print '-Crea Account-'
     nome = raw_input("Digita il Nome per il nuovo Account: ")
     print "=== Passa la Carta o lo Smartphone sul Lettore ==="
     uid = leggi_carta()
@@ -43,46 +39,46 @@ def creaUtente():
     cur.execute("INSERT INTO utenti (uid, nome_utente, punti) VALUES (%s,%s,%s)",(uid, nome, 25))
     db.commit()
     cur.close()
-    print '-Utente Creato-'
+    print '-Account Creato-'
     time.sleep(1)
     menu()
     
 def eliminaUtente():
-    print '-Elimina Utente-'
+    print '-Elimina Account-'
     print "=== Passa la Carta o lo Smartphone sul Lettore ==="
     uid = leggi_carta()
     cur = db.cursor()
     cur.execute("DELETE FROM utenti WHERE uid=%s", (uid))
     db.commit()
     cur.close()
-    print '-Utente Eliminato-'
+    print '-Account Eliminato-'
     time.sleep(1)
     menu()
 
 def gestisciPunti():
-    print '-Aggiorna Punti Utente-'
+    print '-Aggiorna Crediti Utente-'
     print "=== Passa la Carta o lo Smartphone sul Lettore ==="
     uid = leggi_carta()
     cur = db.cursor()
     cur.execute("SELECT punti FROM utenti WHERE uid=%s",(uid))
     puntiLetti = cur.fetchone()
     print puntiLetti[0]
-    punti = input("Inserisci i punti: ")
+    punti = input("Inserisci Crediti: ")
     puntiTot = (puntiLetti[0]+punti)
     cur.execute("UPDATE utenti SET punti=%s WHERE uid=%s",(puntiTot, uid))
     db.commit()
     cur.close()
     time.sleep(1)
-    print str(puntiTot)+(" FabCR associati al Profilo")
+    print str(puntiTot)+(" FabCR associati al Profilo Utente")
     time.sleep(2)
     menu()
 
 def menu():
     print chr(27) + "[2J" + chr(27) + "[1;1H"
     print "FabCR Manager MENU: \n"
-    print '[1] Crea Utente'
-    print '[2] Elimina Utente'
-    print '[3] Gestisci Punti'
+    print '[1] Crea Account'
+    print '[2] Elimina Account'
+    print '[3] Gestisci Crediti'
     options[input('\n Inserisci Scelta menu: ')]()
     
      
@@ -90,9 +86,9 @@ options = {1: creaUtente,
            2: eliminaUtente,
            3: gestisciPunti,
 }
-print '[1] Crea Utente'
-print '[2] Elimina Utente'
-print '[3] Gestisci Punti'
+print '[1] Crea Account'
+print '[2] Elimina Account'
+print '[3] Gestisci Crediti'
 
 options[input('\n Inserisci Scelta menu: ')]()
 
